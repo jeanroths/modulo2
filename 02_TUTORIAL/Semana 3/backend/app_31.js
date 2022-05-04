3428cd7a29f799ca7a6f2cd895dd8716508f3d71
jeanroths <jean.machado@sou.inteli.edu.br>
2022-05-04 09:23:10 -0300

    last commit

--- 02_TUTORIAL/Semana 3/backend/app_31.js
+++ 02_TUTORIAL/Semana 3/backend/app_31.js
@@ -1,20 +1,11 @@
-const http = require('http');
-const fs   = require("fs")
+
+const express = require('express'); 
+const app = express();
+
 const hostname = '127.0.0.1';
 const port = 3031;
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/html');
-  fs.readFile('../frontend/index.html', null, function (error, data) {
-      if (error) {
-          res.writeHead(404);
-          res.write('Whoops! File not found!');
-      } else {
-          res.write(data);
-      }
-      res.end();
-  });
-});
-server.listen(port, hostname, () => {
+app.use(express.static("../frontend/"));
+
+app.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
 });
