ea839fbcd31987d9307e48e0006bdd05dc849c57
jeanroths <jean.machado@sou.inteli.edu.br>
2022-05-10 13:17:51 -0300

    curriculo com JS

---
+++ 04_AUT_EST_EX_OBRIGATORIOS/Semana 4/curriculoEdit1.js
@@ -0,0 +1,10 @@
+console.log("oiii");
+var hptxt = 'Linkedin';
+var url = 'https://www.linkedin.com/in/jean-lucas-rothstein-machado-345872231/'; console.log("acesse meu" + hptxt.link(url));
+
+document.onreadystatechange = function () {
+    if (document.readyState == "complete") {
+        console.log('oi');
+        document.getElementById("lista").innerHTML += "<li>Acesse meu <a href="+url+">"+hptxt+"</a></li>"; 
+    }
+};
