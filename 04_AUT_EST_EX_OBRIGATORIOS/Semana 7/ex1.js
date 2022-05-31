687ec2054a29bcb454ef5b2e39b714531f05eaa4
jeanroths <jean.machado@sou.inteli.edu.br>
2022-05-31 13:02:00 -0300

    ex1

--- 04_AUT_EST_EX_OBRIGATORIOS/Semana 7/ex1.js
+++ 04_AUT_EST_EX_OBRIGATORIOS/Semana 7/ex1.js
@@ -1,20 +1,30 @@
 //declaração de variáveis
 var qtde = 0;
+var op = "x"
 
 $(document).ready(function(){
     console.log("oi");
 });
  
-qtde = Number($("input[id=qtd]").val());
+$("#qtd").focusout(function(){
+    qtde = Number($("input[id=qtd]").val());
+    console.log(qtde);
+});
 
 $("button[id=adc]").click(function(){
-    qtde ++;
-    $("#main").append("<br/> "+qtde+" ");
+    op = "+"
+    if (op == "+"){
+        qtde++;
+        $("#main").append("<br/> "+qtde+" ");
+    }
 });
 
 $("button[id=ret]").click(function(){
-    qtde -= 1;
-    $("#main").append("<br/> "+qtde+" ");
+    op = "-"
+    if (op == "-"){
+        qtde -= 1;
+        $("#main").append("<br/> "+qtde+" ");
+    }
 });
 
 
